d34649b908c49f46798ea5033c7ab9ae05556a05
Siva Srinivasa Rao Mothukuri (simothuk) <simothuk@cisco.com>
2023-11-02 11:22:09 +0530

    Initial Commit

---
+++ src/app.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.body);
+root.render(<h2>Test Check !!! Hello World, This is React Js</h2>);
